0e649364568b6c801704ec45b8a14a98950a2f07
cameronpoe <cameronmpoe@gmail.com>
2022-06-29 12:33:53 -0500

    edits

--- phantoms/xcat_topas_pet_examples/binary_reader.py
+++ phantoms/xcat_topas_pet_examples/binary_reader.py
@@ -1,18 +1,28 @@
 import struct
 
 ## read in test binary
-binary = open("C:/Users/camer/Desktop/xcat_full_body.out_act_1.bin", 'rb')
+binary = open("C:/Users/camer/xcat/xcat_final.out_act_1.bin", 'rb')
 out = open("binary.out", 'w')
 binary.seek(0,2) ## seeks to the end of the file (needed for getting number of bytes)
 num_bytes = binary.tell() ## how many bytes are in this file is stored as num_bytes
 # print num_bytes
 binary.seek(0) ## seeks back to beginning of file
+
+tissues_dict = {}
+
 i = 0 ## index of bytes we are on
 while i < num_bytes:
     binary_data = binary.read(4) ## reads in 4 bytes = 8 hex characters = 32-bits
     i += 4 ## we seeked ahead 4 bytes by reading them, so now increment index i
-    unpacked = struct.unpack("<f", binary_data) ## <f denotes little endian float encoding
-    out.write(str(unpacked) + "\n")
+    activity = struct.unpack("<f", binary_data)[0] ## <f denotes little endian float encoding
+
+    if not activity in tissues_dict:
+        tissues_dict[activity] = 1
+    else:
+        tissues_dict[activity] += 1
+
+for key, value in sorted(tissues_dict.items()):
+    out.write(str(key) + ': ' + str(value) + '\n')
 
 binary.close()
 out.close()
